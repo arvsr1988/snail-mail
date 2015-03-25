c12ec707eefb8ee92a137668dab7dc104d45c6af
Arvind <arvsr2002@gmail.com>
2015-03-25 08:08:45 +0530

    Arvind : incorporating cache bursting of the javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserify = require('gulp-browserify'),
-    concat = require('gulp-concat');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var runSequence = require('run-sequence');
+var rimraf = require('gulp-rimraf');
+var rev = require("gulp-rev");
+var revReplace = require("gulp-rev-replace");
+
+var appDependencies = require('./package.json').dependencies;
 var buildDir = 'dist';
 var publicDir = buildDir + '/public';
+
 gulp.task('sass', function(){
-    gulp.src('sass/*.scss')
+    return gulp.src('sass/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(publicDir))
 });
 
 gulp.task('browserify', function(){
-    gulp.src('js/*.js')
+     return gulp.src('js/*.js')
         .pipe(browserify({
             "transform": ["hbsfy"]
             }
@@ -20,9 +27,6 @@ gulp.task('browserify', function(){
         .pipe(gulp.dest(publicDir))
 });
 
-var rimraf = require('gulp-rimraf');
-
-var appDependencies = require('./package.json').dependencies;
 gulp.task('copy', function(){
    gulp.src('js/plugins/**/*.js')
        .pipe(gulp.dest(publicDir + '/plugins/'));
@@ -41,9 +45,23 @@ gulp.task('clean', function() {
         .pipe(rimraf());
 });
 
-//Convenience task for running a one-off build
-gulp.task('build', ['clean'],  function() {
-    gulp.run('copy', 'browserify', 'sass');
+gulp.task("revision", function(){
+    return gulp.src([publicDir + "/**/*.css", publicDir +  "/**/*.js"])
+        .pipe(rev())
+        .pipe(gulp.dest(publicDir))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest(publicDir))
+});
+
+gulp.task("revreplace", ['revision'], function(){
+    var manifest = gulp.src(publicDir + "/rev-manifest.json");
+    return gulp.src(buildDir + "/views/**/*")
+        .pipe(revReplace({ manifest : manifest}))
+        .pipe(gulp.dest(buildDir + "/views/"));
+});
+
+gulp.task('build',  function(callback) {
+    runSequence('clean', ['copy', 'browserify', 'sass'], 'revreplace', callback);
 });
 
 gulp.task('watch', function() {
