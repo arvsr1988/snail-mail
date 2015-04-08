3cb7e322d35b4fc4d6109ca080ab8bbca572f128
Arvind <arvsr2002@gmail.com>
2015-04-08 10:37:23 +0530

    Arvind : showing smtp in a hidden div.

---
+++ js/email_sending/send_via_smtp.js
@@ -0,0 +1,51 @@
+var responseTemplate = require('../../views/send-email-response.hbs');
+var Handlebars = require("hbsfy/runtime");
+Handlebars.registerPartial('send_email_status', require('../../views/partials/send_email_status.hbs'));
+
+var handleSendEmailResponse = function(data){
+    $(".smtp-submit-element").toggle();
+    if(data.successful){
+        document.forms["smtp-account-details"].reset();
+    }
+    $("#smtp-email-response").html(responseTemplate(data)).show();
+    $("body").scrollTop($("#smtp-email-response").offset().top - 5);
+    return false;
+};
+
+var bindSmtpFormSubmit = function(){
+    var sendEmails = function () {
+        event.preventDefault();
+        $("#send-email-response").html('').hide();
+        $(".smtp-submit-element").toggle();
+        $.ajax({
+            type : 'POST',
+            url: '/send-email',
+            data: $("#attribute-details").serialize() + '&' +  $("#email-details-form").serialize() + '&' + $("#smtp-account-details").serialize(),
+            success: function (data) {
+                handleSendEmailResponse(data);
+            }
+        });
+        return false;
+    };
+    $("#smtp-account-details").isHappy({
+        fields: {
+            '#smtpAddress': {required: true, message: 'Please enter an smtp address'},
+            '#senderEmail' : {required: true, message: 'Please enter a valid email address'},
+            '#smtpUserName': {required: true, message: 'Enter a user name'},
+            '#smtpPassword': {required: true, message: 'Enter a password'}
+        },
+        happy: sendEmails
+    });
+};
+
+var initialiseSmtpFormToggle = function(){
+    $("#smtp-form-header").click(function(){
+        $("#smtp-form-body").toggle();
+    });
+};
+
+var init = function(){
+    initialiseSmtpFormToggle();
+    bindSmtpFormSubmit();
+};
+exports.init = init;
