c31f10eae0aac4704b20bd9180bbdeb9d6c8a367
Arvind <arvsr2002@gmail.com>
2016-04-29 23:25:44 +0530

    Arvind : putting the script to create a deployable artifact in the codebase

---
+++ prepare_snail_deploy.sh
@@ -0,0 +1,15 @@
+#bin/bash
+DEPLOY_DIR=../snail-deploy
+SOURCE_DIR=.
+
+echo $DEPLOY_DIR
+echo $SOURCE_DIR
+
+rm -rf $SOURCE_DIR/dist/
+rm -rf $DEPLOY_DIR/*
+mkdir -p $DEPLOY_DIR
+cd $SOURCE_DIR && npm run create-artifact
+cp $SOURCE_DIR/package.json $DEPLOY_DIR/
+cp -rf $SOURCE_DIR/dist/* $DEPLOY_DIR/
+cd $DEPLOY_DIR
+ln -s app.js $DEPLOY_DIR/server.js
