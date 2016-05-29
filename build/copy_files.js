540694d34c13a756fc5c61aac37c42cb3b103c2e
Arvind <arvinds@sahaj.com>
2016-05-29 10:51:22 +0530

    Arvind : removing the copy of node modules for deployment as it will autoinstall

--- build/copy_files.js
+++ build/copy_files.js
@@ -2,13 +2,7 @@ var shell = require('shelljs');
 var configs = require('./configs.js');
 
 var artifactDir = configs.artifactDir;
-shell.mkdir('-p', artifactDir + "node_modules");
-var dependenciesStr = require('../package.json').dependencies;
-for(var dependency in dependenciesStr){
-   var dependencyDirectory = 'node_modules/' + dependency;
-   shell.mkdir(artifactDir + dependencyDirectory);
-   shell.cp('-rf',dependencyDirectory, artifactDir + 'node_modules/');
-}
+shell.mkdir('-p', artifactDir);
 
 ['public','views','app','routes','js'].forEach(function(directory){
    shell.cp('-rf', directory, artifactDir + directory);
