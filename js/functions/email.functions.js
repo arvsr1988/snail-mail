1bc10d73de928bbe2403a295406781989b1ff2ba
Arvind <arvsr2002@gmail.com>
2018-08-11 20:21:42 +0530

    removing trimming of emails on validate

--- js/functions/email.functions.js
+++ js/functions/email.functions.js
@@ -18,6 +18,6 @@ module.exports = {
 
     isValidEmail : function(emailAddress){
         var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        return pattern.test((emailAddress || '').trim());
+        return pattern.test(emailAddress);
     }
 };
